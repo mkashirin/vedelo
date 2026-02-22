4b6c82f89fb092d7343dd47052a859936a99c4c1
Michael Kashirin <mail.mkashirin@yandex.ru>
2026-02-22 23:10:41 +0300

    Add new static

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ LD_LIBRARY_PATH=$LIBTORCH:$LD_LIBRARY_PATH \
     -m artifacts/Vedelo-V1S/weights/epoch40.torchscript \
     -s dataset/test/video_test.mp4 \
     -d video_test_pred.mp4 \
-    --conf 0.5 \
+    --conf 0.6 \
     --iou-thresh 0.2 \
     --max-age 90
 
