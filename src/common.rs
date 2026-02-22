a2f1c708b3a955c554af63f68fd9abc4f6c136fe
Michael Kashirin <mail.mkashirin@yandex.ru>
2026-02-22 19:53:49 +0300

    Adapt opencv::imgproc::cvt_color to be compatible with various OpenCV versions

--- src/common.rs
+++ src/common.rs
@@ -1,7 +1,7 @@
 use crate::tracker::Track;
 use anyhow::Result;
 use opencv::{
-    core::{Point, Rect, Scalar},
+    core::{AlgorithmHint, Point, Rect, Scalar},
     imgproc,
     prelude::*,
 };
@@ -55,4 +55,26 @@ pub fn draw_prediction(frame: &mut Mat, obj: &Track) -> Result<()> {
     )?;
 
     Ok(())
+}
+
+#[cfg(target_os = "macos")]
+pub fn cvt_color_wrapper(
+    src: &Mat,
+    dst: &mut Mat,
+    code: i32,
+    dst_cn: i32,
+) -> opencv::Result<()> {
+    // MacOS/Newer OpenCV requires 5 arguments.
+    imgproc::cvt_color(src, dst, code, dst_cn, AlgorithmHint::ALGO_HINT_DEFAULT)
+}
+
+#[cfg(target_os = "linux")]
+pub fn cvt_color_wrapper(
+    src: &Mat,
+    dst: &mut Mat,
+    code: i32,
+    dst_cn: i32,
+) -> opencv::Result<()> {
+    // Linux/Older OpenCV requires 4 arguments.
+    imgproc::cvt_color(src, dst, code, dst_cn)
 }
--- src/detector.rs
+++ src/detector.rs
@@ -7,6 +7,8 @@ use opencv::{
 use std::ops::Div;
 use tch::{Device, Kind, Tensor};
 
+use crate::common::cvt_color_wrapper;
+
 #[derive(Debug, Clone)]
 pub struct Detection {
     pub rect: Rect,
@@ -157,7 +159,7 @@ impl YoloDetector {
         )?;
 
         let mut rgb = Mat::default();
-        imgproc::cvt_color(&padded, &mut rgb, imgproc::COLOR_BGR2RGB, 0)?;
+        cvt_color_wrapper(&padded, &mut rgb, imgproc::COLOR_BGR2RGB, 0)?;
 
         let size = rgb.size()?;
         let h = size.height;
